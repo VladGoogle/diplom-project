bb44972e7d2c761a711888117d967efd42122923
Yakov <Oridinalscail@gmail.com>
2023-04-12 14:33:03 +0300

    front-end update

---
+++ src/helpers/projectList.js
@@ -0,0 +1,96 @@
+import card_image01 from "./../img/card_image.png"
+
+const cards = [
+    {
+        id:1,
+        status: "DISCOUNT",
+        category: "Smartphone",
+        title: "iPhone 14",
+        img: card_image01,
+        old_price: "499$",
+        new_price: "299$"
+    },
+    {
+        id:2,
+        status: "DISCOUNT",
+        category: "Smartphone",
+        title: "iPhone 14",
+        img: card_image01,
+        old_price: "499$",
+        new_price: "299$"
+    },
+    {
+        id:3,
+        status: "DISCOUNT",
+        category: "Smartphone",
+        title: "iPhone 14",
+        img: card_image01,
+        old_price: "499$",
+        new_price: "299$"
+    },
+    {
+        id:4,
+        status: "DISCOUNT",
+        category: "Smartphone",
+        title: "iPhone 14",
+        img: card_image01,
+        old_price: "499$",
+        new_price: "299$"
+    },
+    {
+        id:5,
+        status: "DISCOUNT",
+        category: "Smartphone",
+        title: "iPhone 14",
+        img: card_image01,
+        old_price: "499$",
+        new_price: "299$"
+    },
+    {
+        id:6,
+        status: "DISCOUNT",
+        category: "Smartphone",
+        title: "iPhone 14",
+        img: card_image01,
+        old_price: "499$",
+        new_price: "299$"
+    },
+    {
+        id:7,
+        status: "DISCOUNT",
+        category: "Smartphone",
+        title: "iPhone 14",
+        img: card_image01,
+        old_price: "499$",
+        new_price: "299$"
+    },
+    {
+        id:8,
+        status: "DISCOUNT",
+        category: "Smartphone",
+        title: "iPhone 14",
+        img: card_image01,
+        old_price: "499$",
+        new_price: "299$"
+    },
+    {
+        id:9,
+        status: "DISCOUNT",
+        category: "Smartphone",
+        title: "iPhone 14",
+        img: card_image01,
+        old_price: "499$",
+        new_price: "299$"
+    },
+    {
+        id:10,
+        status: "DISCOUNT",
+        category: "Smartphone",
+        title: "iPhone 14",
+        img: card_image01,
+        old_price: "499$",
+        new_price: "299$"
+    }
+] 
+
+export {cards}
