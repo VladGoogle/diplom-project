e8305c3593c3f972682f9a634b992bba2d78add6
Yakov <Oridinalscail@gmail.com>
2023-05-19 03:10:08 +0300

    wallet update

--- client/src/components/settingsTabs/Wallet/AddedWallet.jsx
+++ client/src/components/settingsTabs/Wallet/AddedWallet.jsx
@@ -1,8 +1,55 @@
 import "./style.css"
+import React from "react";
 import mastercard from "../../../img/mastercard_big.svg"
+import WalletForm from "./WalletForm";
+import { useState, useEffect } from "react";
+import createAxiosInstance from "../../../utils/axios/instance";
 
 const AddedWallet = () => {
+
+    const instance = createAxiosInstance();
+    const [items, setItems] = React.useState({});
+    const [showForm, setShowForm] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await instance.get("/user/getByToken");
+            setItems(response.data.card);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchData();
+      }, []);
+
+      const handleRemoveWallet = async () => {
+        try {
+            await instance.delete('/account/card/1');
+            setItems({});
+            setShowForm(true);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+
+        
+      const maskCardNumber = (number) => {
+        if (!number) {
+          return ""; // Возвращаем пустую строку, если номер карточки не определен или пустой
+        }
+        
+        const maskedNumber = number.substring(0, 12).replace(/./g, "*") + number.substring(12);
+        return maskedNumber;
+      };
+
     return ( 
+        <>
+        {showForm ? (
+            <WalletForm />
+        ) : (
         <div className="added__wallet">
             <div className="added__wallet-top">
                 <span className="added__wallet-bankname">
@@ -11,17 +58,19 @@ const AddedWallet = () => {
                 <img src={mastercard} alt="mastercard" />
             </div>
             <span className="added__wallet-middle">
-                **** **** **** 8493
+            {maskCardNumber(items.number)}
             </span>
             <div className="added__wallet-bottom">
                 <span className="added__wallet-change">
                     Change
                 </span>
-                <svg className="added__wallet-delete" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg onClick={() => handleRemoveWallet()} className="added__wallet-delete" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M6 19C6 19.5304 6.21071 20.0391 6.58579 20.4142C6.96086 20.7893 7.46957 21 8 21H16C16.5304 21 17.0391 20.7893 17.4142 20.4142C17.7893 20.0391 18 19.5304 18 19V7H6V19ZM8 9H16V19H8V9ZM15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5Z" fill="#ED2D2D" fill-opacity="0.7"/>
                 </svg>
             </div>
         </div>
+        )}
+        </>
      );
 }
  
---
+++ client/src/components/settingsTabs/Wallet/WalletForm.jsx
@@ -0,0 +1,105 @@
+import "./style.css"
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import createAxiosInstance from "../../../utils/axios/instance";
+import AddedWallet from "./AddedWallet";
+import { useState } from "react";
+
+const schema = yup.object().shape({
+    number: yup
+    .string()
+    .required('Card number is required'),
+    expMonth: yup
+      .number()
+      .integer('Month must be a number')
+      .required('Month is required'),
+    expYear: yup
+      .number()
+      .integer('Year must be a number')
+      .required('Year is required'),
+    cvc: yup
+      .number()
+      .integer('CVC must be a number')
+      .required('CVC is required'),
+    currency: yup
+      .string()
+      .oneOf(['USD', 'UAH'], 'Invalid currency')
+      .required('Currency is required')
+      .default('USD'),
+  });
+
+const WalletForm = () => {
+
+    const instance = createAxiosInstance();
+    const [showForm, setShowForm] = useState(false);
+
+    
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+      });
+
+      const onSubmit = async (data) => {
+        try {
+          await schema.validate(data);
+          const requestData = { ...data, currency: 'USD' };
+          const response = await instance.post('/account/card', data);
+          console.log(response.data);
+          setShowForm(true);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      
+
+    return (
+      <>
+            {showForm ? (
+              <AddedWallet /> 
+            ) : (
+              <>
+            <form className="wallet" onSubmit={handleSubmit(onSubmit)}>
+                <div className="wallet__inputs">
+                    <div className="wallet__input-top">
+                        <input
+                        placeholder="Card number"
+                        name="number"
+                        type="text"
+                        id="number"
+                        className="wallet__input wallet__input-card"
+                        {...register('number')}
+                    />
+                    {errors.number && <span className="error-message">{errors.number.message}</span>}
+                    </div>
+                    <div className="wallet__input-bottom">
+                        <div className="wallet__input-expired">
+                            <div className="expMonth_box">
+                            <input placeholder="MM" type="tel" name="expMonth" id="expMonth" className="wallet__input wallet__input-expMonth" {...register('expMonth')}/>
+                            {errors.expMonth && <span className="error-message">{errors.expMonth.message}</span>}
+                            </div>
+                            <span className="wallet__inner">/</span>
+                            <div className="expYear_box">
+                            <input placeholder="YY" type="tel" name="expYear" id="expYear" className="wallet__input wallet__input-expYear" {...register('expYear')} />
+                            {errors.expYear && <span className="error-message">{errors.expYear.message}</span>}
+                            </div>
+                        </div>
+                        <div className="cvc_box">
+                        <input placeholder="CVV" type="tel" name="cvc" id="cvc" className="wallet__input wallet__input-cvv" {...register('cvc')}/>
+                        {errors.cvc && <span className="error-message">{errors.cvc.message}</span>}
+                        </div>
+                    </div>
+                    <button type="submit" className="wallet__button-submit">
+                        SAVE CARD
+                    </button>
+                </div>
+            </form>
+            <p className="wallet__additional-information">
+            By clicking the "SAVE CARD" button, you confirm that you give your consent (acceptance) to the provision of financial services for the transfer of funds for the purpose of paying for the order/services on La Pigeon by one of the following financial partners:- JSC "TASKOMBANK", JSC "ALFA-BANK", JSC "PUMB", JSC KB "PRIVATBANK", JSC "OSCHADBANK"- LLC "FC" "EVO" (more details →)
+            </p>
+            </>
+            )}
+          </>
+    );
+}
+
+export default WalletForm;
